617b83bae75839bae1f2a1d7c900f52be730ccf4
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-25 19:11:04 +0300

    added random id

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,36 @@
-let title, description, task_createdAt, today, task_point;
+let task_title, task_description, task_createdAt, today, task_point;
+var task_id = 0;
 let todos = JSON.parse(localStorage.getItem("todo-list"));
 
 // check if input is empty or not
 function validateInput(input){
-    if(input.length>0) {
+    if(input.length > 0) {
         return true;
     }
     return false;
 }
 
+function generateTaskId() {
+    return Math.floor(Math.random() * 1000);
+}
+
 $("#add_task").on( 'click', function () {
+
+
     task_title =  $("#title").val();
     task_description = $("#description").val();
     today = new Date();
     task_point = $("#point").val();
     task_createdAt = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+ (today.getHours() % 12 || 12) + ":" + today.getMinutes() + ":" + today.getSeconds();
-    console.log(task_description);
-    if(validateInput(title) && validateInput(description)){
-        let taskInfo = {title: task_title, description: task_description, point: task_point, IsDone: false, CreatedAt: task_createdAt };
+    task_id = generateTaskId();
+
+    if(validateInput(task_title)){
+        todos = !todos ? [] : todos;
+        let taskInfo = {id: task_id, title: task_title, description: task_description, point: task_point, IsDone: false, CreatedAt: task_createdAt };
         todos.push(taskInfo);
-    }else{
-        alert("Title & description are required");
+    } else {
+        alert("Title , Points & description are required");
     }
+    localStorage.setItem("todo-list", JSON.stringify(todos));
 });
+console.log(todos);
