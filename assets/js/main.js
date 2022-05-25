f94244988f1a2d910baecf633988650c57c690d0
samah-basheer <samahsharafbasheer@gmail.com>
2022-05-25 17:31:48 +0300

    added task points

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-let title, description;
+let title, description, task_createdAt, today, task_point;
+let todos = JSON.parse(localStorage.getItem("todo-list"));
 
 // check if input is empty or not
 function validateInput(input){
@@ -9,12 +10,16 @@ function validateInput(input){
 }
 
 $("#add_task").on( 'click', function () {
-    title =  $("#title").val();
-    description = $("#description").val();
+    task_title =  $("#title").val();
+    task_description = $("#description").val();
+    today = new Date();
+    task_point = $("#point").val();
+    task_createdAt = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' '+ (today.getHours() % 12 || 12) + ":" + today.getMinutes() + ":" + today.getSeconds();
+    console.log(task_description);
     if(validateInput(title) && validateInput(description)){
-
+        let taskInfo = {title: task_title, description: task_description, point: task_point, IsDone: false, CreatedAt: task_createdAt };
+        todos.push(taskInfo);
     }else{
         alert("Title & description are required");
     }
 });
-
